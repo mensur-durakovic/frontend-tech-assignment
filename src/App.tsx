4bac6b80d0e874d85148dc365a0af92ee1bdd73a
Mensur <mensurdurakovic5@gmail.com>
2022-09-17 13:56:55 +0200

    setup project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
-import family from './family.svg';
+import HomePage from './pages/home';
 
 function App() {
   return (
-    <main>
-     <div 
-        style={{background: "#173F4B", width: "130px", height: "136px", boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.25)"}}>
-          <img src={family} alt="Family" />
-          <p style={{textAlign: "center"}}>Baby's birth</p>
-     </div>
-    </main>
+    <HomePage/>
   );
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <main className='home-page'>
+      home page
+    </main>
+  )
+}
